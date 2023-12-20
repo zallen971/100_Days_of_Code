1170e813adbd63c510fe829da3f4b9aa3afa96c0
Zachary Allen <zachary.allen2@snhu.edu>
2023-12-20 13:10:49 -0500

    Changed location of logo import.
    Created a while loop on whether user wants to continue.

--- 100 Days of Code Projects/Caesar Cypher/caesar_cipher.py
+++ 100 Days of Code Projects/Caesar Cypher/caesar_cipher.py
@@ -6,22 +6,33 @@ def caesar(message, shift_num, ciph_direction):
     text_message = ""
     if ciph_direction == "decode":
         shift_num *= -1
-    for letter in message:
-        position = alphabet.index(letter)
-        new_position = position + shift_num
-        text_message += alphabet[new_position]
+    for char in message:
+        if char in alphabet:
+            position = alphabet.index(char)
+            new_position = position + shift_num
+            text_message += alphabet[new_position]
+        else:
+            text_message += char
     print(f"The encrypted text is: {text_message}")
-
-# takes input for encode/decode, the messane, and the shift number
-direction = input("Type 'encode' to encrypt. Type 'decode' to decrypt:\n")
-text = input("Type your message:\n")
-shift = int(input("Type the shift number:\n"))
-
 
 #The logo
 from art import logo
 print(logo)
+should_continue = True
 
+# Creates a while loop on whether the user wants to continue
+# Takes input for encode/decode, the messane, and the shift number
+while should_continue:
+    direction = input("Type 'encode' to encrypt. Type 'decode' to decrypt:\n")
+    text = input("Type your message:\n")
+    shift = int(input("Type the shift number:\n"))
 
-#Calls the function that corresponds with the users direction input
-caesar(message=text, shift_num=shift, ciph_direction=direction)
+    # Makes sure the shift is within the number of the alphabet
+    shift = shift % 26
+    caesar(message=text, shift_num=shift, ciph_direction=direction)
+
+    # Asks if the user wants to go again or not and exits if no
+    result = input("Type 'yes' if you want to go again, or type 'no' to exit.\n")
+    if result == "no":
+        should_continue = False
+        print("now Exiting...")
